c7ef72985423bcb5bc0526d820aecd79b56e0cb5
Jose Antonio Alvarez <jaam@jaam.eu>
2020-01-20 22:40:40 +0100

    Logger component now uses the correct logging mechanism

--- src/main/java/org/flowable/examples/spring/boot/util/Logger.java
+++ src/main/java/org/flowable/examples/spring/boot/util/Logger.java
@@ -1,15 +1,20 @@
 package org.flowable.examples.spring.boot.util;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
  * Sample class to output to console
+ *
+ * @author Jose Antonio Alvarez
  */
 @Component
 public class Logger {
 
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(Logger.class);
+
     public void info(String text) {
-        System.out.println(text);
+        LOG.info(text);
     }
 
 }
